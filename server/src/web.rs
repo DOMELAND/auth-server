1525556cf9dc5b98edd2c7633a4b2c1ec3fc01a5
maxatbj <iamliqiang@gmail.com>
2022-04-20 12:32:19 +0800

    add ethaddr query

--- server/src/web.rs
+++ server/src/web.rs
@@ -80,7 +80,6 @@ fn username_to_uuid(req: &Request) -> Result<Response, AuthError> {
     let body = req.data().unwrap();
     let payload: UuidLookupPayload = serde_json::from_reader(body)?;
     let uuid = auth::username_to_uuid(&payload.username)?;
-    let response = UuidLookupResponse { uuid, ethaddr };
     Ok(Response::json(&response))
 }
 
